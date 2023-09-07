56cc953b72c16f34394f69482939ac032edf8730
lakshmidurga14 <84463376+lakshmidurga14@users.noreply.github.com>
2023-09-07 11:15:58 +0530

    L=int(input()) N=int(input()) for i in range(N):     W,H=map(int,input().split())     if L>W or L>H:         print("UPLOAD ANOTHER")     elif W==H:         print("ACCEPTED")     else:         print("CROP IT")

---
+++ Roy_and_Profile_Picture.py
@@ -0,0 +1,11 @@
+L=int(input())
+N=int(input())
+for i in range(N):
+    W,H=map(int,input().split())
+    if L>W or L>H:
+        print("UPLOAD ANOTHER")
+    elif W==H:
+        print("ACCEPTED")
+    else:
+        print("CROP IT")
+    
